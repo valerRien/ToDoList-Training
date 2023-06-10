6b1afb60d7ac007db50765dd6d3427a56aa42ca8
valerRien <valeracedin@yandex.ru>
2023-06-10 14:32:51 +0300

    added openAPI description for controllers

--- src/main/java/pet/todolist/controller/UserController.java
+++ src/main/java/pet/todolist/controller/UserController.java
@@ -1,5 +1,9 @@
 package pet.todolist.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,26 +28,31 @@ public class UserController {
     }
 
     @GetMapping("/all")
+    @Operation(description = "Show all users")
     public ResponseEntity<List<UserEntity>> getUsers() {
         return new ResponseEntity<>(userService.getUsers(), OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Show one user Info")
     public ResponseEntity<UserEntity> getUser(@PathVariable(name = "id")int id) {
         return new ResponseEntity<>(userService.getUser(id), OK);
     }
 
     @PostMapping
+    @Operation(description = "Create new user")
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
         return new ResponseEntity<>(userService.createUser(user), OK);
     }
 
     @PutMapping
+    @Operation(description = "Update user info")
     public ResponseEntity<UserEntity> updateUser(@RequestBody UserEntity user) {
         return new ResponseEntity<>(userService.updateUser(user), OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Delete user")
     public HttpStatus deleteUser(@PathVariable int id) {
         userService.delete(id);
         return OK;
